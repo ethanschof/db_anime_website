8b64a7cd02625831b34edcead75fcd087ee98c9c
ethanschof <70534200+ethanschof@users.noreply.github.com>
2022-05-03 10:21:49 -0600

    Create header and footer include .php, create charinfo.php
    Creates the basic header and footer include .php files.  Creates the characterinfo.php and index.php file.

---
+++ characterinfo.php
@@ -0,0 +1,55 @@
+<?php require '/includes/header.php' ?>
+<?php
+$servername = "localhost";
+$username = "Student";
+$password = "CompSci364";
+$db_name = "anime-website"
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $db_name);
+
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+// `characters` (`characterID`, `name`, `gender`, `bio`, `status`, `animeID`, `ship`, `va`, `image`)
+// Get the ID from the URL
+$id = $_GET[characterID];
+
+// Prepare the sql query
+$query = $conn->prepare('SELECT * FROM characters WHERE characterID = ?');
+
+// This says we are expecting id to be an integer, use s for string
+$query->bind_param("i", $id)
+
+// Run the query
+$query->execute()
+
+// Bind the result variables
+$query->bind_result($characterID, $name, $gender, $bio, $status, $animeID, $ship, $va, $image)
+
+echo '<section class="descriptionAnime">
+  <div class="descriptionAnime__left">
+    <h3 class="descriptionAnime__left-title">' . $name . '</h3>
+    <img src="'. $image .'" alt="'. $name .'" title="'. $name .'">
+    <div class="descriptionAnime__Information">
+      <h3>Information</h3>
+      <p>Gender: <span>';
+      if ($gender == "M"){
+        echo 'Male';
+      }
+      else {
+        echo 'Female';
+      }
+      echo '</span></p>
+    </div>
+  </div>
+  <div class="descriptionAnime__right">
+    <h3>Bio</h3>
+    '. $bio .'
+</div>
+</section>'
+
+?>
+<?php require '/includes/footer.php' ?>
---
+++ includes/header.php
@@ -0,0 +1,21 @@
+<?php
+echo
+'<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Anime Database</title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <!--Start menu header -->
+    <h1>My Cool Anime Website</h1>
+    <nav>
+      <a href="index.php">Home</a>
+      <a href="animelist.php">Anime</a>
+      <a href="characterinfo.php?characterID=2">Characters</a>
+      <a href="contactUs.php">Contact Us</a>
+      <a href="userInformation_login.html">My Account</a>
+  <div class="animation start-home"></div>
+</nav>';
+?>
