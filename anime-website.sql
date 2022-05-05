99af52957df8da70835713ff971f00971bfe9742
ethanschof <70534200+ethanschof@users.noreply.github.com>
2022-05-04 19:14:49 -0600

    Update sql dump

--- anime-website.sql
+++ anime-website.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1:3306
--- Generation Time: Apr 27, 2022 at 08:33 PM
+-- Generation Time: May 05, 2022 at 01:13 AM
 -- Server version: 5.7.36
 -- PHP Version: 7.4.26
 
@@ -147,7 +147,7 @@ CREATE TABLE IF NOT EXISTS `befriends` (
 --
 
 INSERT INTO `befriends` (`userID1`, `userID2`) VALUES
-(0, 1);
+(1, 2);
 
 -- --------------------------------------------------------
 
@@ -179,7 +179,7 @@ CREATE TABLE IF NOT EXISTS `characters` (
 INSERT INTO `characters` (`characterID`, `name`, `gender`, `bio`, `status`, `animeID`, `ship`, `va`, `image`) VALUES
 (0, 'Eren Jaegar', 'M', 'Birthdate: March 30\r\nHeight: 183 cm (6\'0\")\r\nWeight: 63 kg (139 lbs)\r\nPosition: 104th Trainees Squad, Scouting Legion\r\nGraduation rank: 5th', 'living', 47, 0, 0, 'https://cdn.myanimelist.net/images/characters/10/216895.jpg'),
 (1, 'Mikasa Ackerman', 'F', 'Age: 15, 19 (after time skip)\r\nBirthdate: February 10\r\nHeight: 170 cm (5\'7\")\r\nWeight: 68 kg (150 lbs)\r\nPosition: 104th Trainees Squad, Scouting Legion\r\nGraduation rank: 1st', 'living', 47, 0, 1, 'https://cdn.myanimelist.net/images/characters/9/215563.jpg'),
-(2, 'Spike Spiegel', 'M', 'Spike Spiegel is a tall and lean 27-year-old bounty hunter born on Mars. The inspiration for Spike is found in martial artist Bruce Lee who uses the martial arts style of Jeet Kune Do as depicted in Session 8, \"Waltz For Venus\". He has fluffy, dark green hair (which is inspired by Yusaku Matsuda\'s) and reddish brown eyes, one of which is artificial and lighter than the other. He is usually dressed in a blue leisure suit, with a yellow shirt and Lupin III inspired boots. A flashback in Session 6 revealed it was his fully functioning right eye which was surgically replaced by the cybernetic one (although Spike himself may not have conscious recollection of the procedure since he claims to have lost his natural eye in an \"accident\"). One theory is that his natural eye may have been lost during the pre-series massacre in which he supposedly \"died\". The purpose of this cybernetic eye is never explicitly stated, though it apparently gives him exceptional hand-eye coordination - particularly with firearms (Spike\'s gun of choice is a Jericho 941, as seen throughout the series). In the first episode, when facing a bounty-head using Red Eye, Spike mocks him, calling his moves \"too slow\". At first, this seems like posturing on Spike\'s part, but even with his senses and reflexes accelerated to superhuman levels by the drug, the bounty cannot even touch Spike. A recurring device throughout the entire show is a closeup on Spike\'s fully-natural left eye before dissolving to a flashback of his life as part of the syndicate. As said by Spike himself in the last episode, his right eye \"only sees the present\" and his left eye \"only sees the past.\" Spike often has a bent cigarette between his lips, sometimes despite rain or \"No Smoking\" signs.\r\n(Source: MAL Rewrite)', '1', NULL, NULL, NULL, 'https://media.kitsu.io/characters/images/2/original.jpg'),
+(2, 'Spike Spiegel', 'M', 'Spike Spiegel is a tall and lean 27-year-old bounty hunter born on Mars. The inspiration for Spike is found in martial artist Bruce Lee who uses the martial arts style of Jeet Kune Do as depicted in Session 8, \"Waltz For Venus\". He has fluffy, dark green hair (which is inspired by Yusaku Matsuda\'s) and reddish brown eyes, one of which is artificial and lighter than the other. He is usually dressed in a blue leisure suit, with a yellow shirt and Lupin III inspired boots. A flashback in Session 6 revealed it was his fully functioning right eye which was surgically replaced by the cybernetic one (although Spike himself may not have conscious recollection of the procedure since he claims to have lost his natural eye in an \"accident\"). One theory is that his natural eye may have been lost during the pre-series massacre in which he supposedly \"died\". The purpose of this cybernetic eye is never explicitly stated, though it apparently gives him exceptional hand-eye coordination - particularly with firearms (Spike\'s gun of choice is a Jericho 941, as seen throughout the series). In the first episode, when facing a bounty-head using Red Eye, Spike mocks him, calling his moves \"too slow\". At first, this seems like posturing on Spike\'s part, but even with his senses and reflexes accelerated to superhuman levels by the drug, the bounty cannot even touch Spike. A recurring device throughout the entire show is a closeup on Spike\'s fully-natural left eye before dissolving to a flashback of his life as part of the syndicate. As said by Spike himself in the last episode, his right eye \"only sees the present\" and his left eye \"only sees the past.\" Spike often has a bent cigarette between his lips, sometimes despite rain or \"No Smoking\" signs.\r\n(Source: MAL Rewrite)', '1', 1, NULL, NULL, 'https://media.kitsu.io/characters/images/2/original.jpg'),
 (3, 'Faye Valentine', 'F', 'One of the members of the bounty hunting crew in the anime series Cowboy Bebop. Often seen with a cigarette and in a revealing outfit complete with bright yellow hot pants and a matching, revealing top, black suspenders, white boots, and a long-sleeved red shirt worn normally through the sleeves, not to mention her signature headband, she is unusually attractive, sporting a bob of violet hair, green eyes, fair skin, and a voluptuous body. Although appearing to be no more than her 23 years alive suggests, Faye is actually upwards of 74-years-old, having been put into cryogenic freeze after a space shuttle accident. During the course of the series (set in 2071), Faye manages to cross paths with Spike and Jet twice before she finally makes herself at home aboard their ship the second time, much to the consternation and disapproval of the two men, both of whom have their own reservations about women in general. Faye herself is brash, egotistical, and quite lazy, despite taking plenty of time to pamper and care for her own appearance. Faye has also been placed under arrest several times in the series and spends much time in handcuffs on the ship. She, at times, expects the boys to take care of bounties for her, while she sits by idly to reap the benefits and eat all their food, another source of conflict.', NULL, 1, NULL, NULL, 'https://cdn.myanimelist.net/images/characters/15/264961.jpg');
 
 -- --------------------------------------------------------
@@ -201,8 +201,8 @@ CREATE TABLE IF NOT EXISTS `favorites` (
 --
 
 INSERT INTO `favorites` (`userID`, `characterID`) VALUES
-(0, 0),
-(1, 1);
+(1, 0),
+(2, 1);
 
 -- --------------------------------------------------------
 
@@ -257,7 +257,7 @@ INSERT INTO `ship` (`characterID0`, `characterID1`, `likes`, `dislikes`) VALUES
 
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE IF NOT EXISTS `users` (
-  `userID` int(11) NOT NULL,
+  `userID` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(30) DEFAULT NULL,
   `password` varchar(25) DEFAULT NULL,
   `email` varchar(50) DEFAULT NULL,
@@ -269,15 +269,15 @@ CREATE TABLE IF NOT EXISTS `users` (
   KEY `friends` (`friends`),
   KEY `animelist` (`animelist`),
   KEY `favorites` (`favorites`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`userID`, `username`, `password`, `email`, `bio`, `friends`, `animelist`, `favorites`) VALUES
-(0, 'bigJD57', 'iamsocool111', 'bigJD57@gmail.com', 'I like anime and anime girls.', 1, 0, 0),
-(1, 'totallyNotMy2ndAcct', 'iamsad1!', 'smallJD5777@gmail.com', 'I need a girlfriend.', 0, 0, 1);
+(1, 'bigJD57', 'iamsocool111', 'bigJD57@gmail.com', 'I like anime and anime girls.', 1, 0, 0),
+(2, 'totallyNotMy2ndAcct', 'iamsad1!', 'smallJD5777@gmail.com', 'I need a girlfriend.', 0, 0, 1);
 
 -- --------------------------------------------------------
 
